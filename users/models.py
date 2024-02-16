8530c03d22804ac39c5314331a9b98deb191541f
Degef Sium <Degei411233@outlook.com>
2024-02-16 16:23:31 +0400

    amran - Committed by Degef Sium on 2024-02-16 16:23:27

--- users/models.py
+++ users/models.py
@@ -19,3 +19,9 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+class user_things(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, default='offline')
+    def __str__(self):
+        return f'{self.user.username} things'
